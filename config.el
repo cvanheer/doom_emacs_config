cf6352b5dae9e4c3e19ac223300d6312996436f3
Christina Van Heer <christinavanheer@gmail.com>
2024-01-16 10:48:44 +1100

    added centaur tabs

--- config.el
+++ config.el
@@ -159,6 +159,22 @@
 (define-hostmode poly-markdown-hostmode
   :mode 'markdown-mode)
 
+(require 'centaur-tabs)
+(after! centaur-tabs
+  (centaur-tabs-mode 1) ; run at startup
+  ;(centaur-tabs-headline-match)
+  (setq centaur-tabs-style "slant"
+        centaur-tabs-height 32
+        centaur-tabs-set-icons t
+        centaur-tabs-gray-out-icons 'buffer
+        centaur-tabs-set-bar 'below
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-modified-marker "*"
+        ;centaur-tabs-change-fonts "Menlo" 120
+        )
+  )
+
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
